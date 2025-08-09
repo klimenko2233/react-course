6e1a76ad295cb59f85abfced03c7e4feebb86afc
aleksadrklimenko <ngk1dnbs@gmail.com>
2025-08-09 22:27:19 +0300

    hw 7.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import {useFetch} from "./hooks/useFetch.ts";
 
 function App() {
+  const users = useFetch<{id:number,name:string}[]>('https://jsonplaceholder.typicode.com/users', []);
   return (
-    <>
-    </>
+    <div>
+      {users.map((user:{id:number,name:string}) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>ID: {user.id}</p>
+        </div>
+      ))
+      }
+    </div>
   )
 }
 
