cc4d9cfd6666fcb2aaffecd7b3448ed79274c20f
aleksadrklimenko <ngk1dnbs@gmail.com>
2025-08-12 11:29:46 +0300

    hw-8.1 start

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import {Outlet} from "react-router";
 
 function App() {
   return (
     <>
+      <Outlet/>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(<App/>)
+import {Provider} from "react-redux";
+import {RouterProvider} from "react-router/dom";
+import {routes} from "./router/routes.tsx";
+import {store} from "./store/store.ts";
+createRoot(document.getElementById('root')!)
+    .render(
+        <Provider store={store}>
+            <RouterProvider router={routes}/>
+        </Provider>
+    );
